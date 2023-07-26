e0693c217b4266b7eb7028f30c32c1c6b0332125
SYN-3r <135239201+SYN-3r@users.noreply.github.com>
2023-07-26 17:54:29 +0000

    Update WayStar.sh

--- WayStar.sh
+++ WayStar.sh
@@ -2,9 +2,11 @@
 
 #WayStar by SYN-3r
 
-#Art
+#FUNCTIONS
+
+#Display art for the menu
+Art() {
 Color=$(printf '\033')
-Sleepy=$(sleep 0.5s)
 Magenta="${Color}[1;95m"
 LGray="${Color}[1;37m"
 DGray="${Color}[1;90m"
@@ -15,34 +17,47 @@ Normal="${Color}[0m"
 Green="${Color}[1;32m"
 UMagenta="${Color}[1;95m${Color}[5m"
 LgBlink="${Color}[1;37m${Color}[5m"
-AltBlink=$(sleep 0.5s)"${Color}[1;37m${Color}[6m"
 
 printf """
-${Magenta}/-----------------------------------------------------------------------------\\
-|                              ${Cyan}WayStar by SYN-3r                              ${Normal}|
-|                         ${Green}A Windows Enummeration Script                       ${Normal}|
+/-----------------------------------------------------------------------------\\
+|                           ${LgBlink}⭐${Normal} ${Cyan}WayStar by SYN-3r ${LgBlink}⭐${Normal}                           ${Normal}|
+|                       ${Cyan}A Windows Enummeration Script                         ${Normal}|
 |-----------------------------------------------------------------------------|
-|  ${Magenta}, : | | ${Cyan}| |:  ${Green}||  :     ${Magenta},  :  |  |+${Cyan}|: | :${Green} : :|   ${LgBlink}.${Normal}        ${Normal},${Cyan}              ${LgBlink}.${Normal} ${Normal}|
-|      ${Magenta}, : | ${Cyan}:|  ||  ${Green}|:  :    ${Magenta},  |  | :${Cyan}| : | : |${Green}:   ${Cyan}|  ${LgBlink}.${Normal}                    : ${Normal}|
-|         ${Magenta}${LgBlink}.${Normal}' ':  ${Cyan}||  |:  ${Green}|  '${Magenta}       , || ${Cyan}| : | ${Green}|: : |   ${LgBlink}.${Normal}  ,           ${Normal}${LgBlink}.${Normal}   ${Magenta}:${LgBlink}.${Normal} ${Normal}|
+|  ${Magenta}, : | | ${Cyan}| |:  ${Green}||  :     ${Magenta},  :  |  |+${Cyan}|: | :${Green} : :|   .        ${Normal},${Cyan}              . ${Normal}|
+|      ${Magenta}, : | ${Cyan}:|  ||  ${Green}|:  :    ${Magenta},  |  | :${Cyan}| : | : |${Green}:   ${Cyan}|  ${LgBlink}.${Normal}                    ${Magenta}: ${Normal}|
+|         ${Magenta}${LgBlink}.${Normal}${Magenta}' ':  ${Cyan}||  |:  ${Green}|  '${Magenta}       , || ${Cyan}| : | ${Green}|: : |   .${Normal}  ,           ${Normal}${LgBlink}.${Normal}   ${Magenta}:. ${Normal}|
 |                ${Magenta},'  ||  ${Cyan}|  ' |   ${Normal}*${Magenta}    , : | | ${Cyan}:| |${Normal}*${Green}|  :   ${Magenta}:               ${Cyan}:${Green}| ${Normal}|
-|        *    *       ${Magenta},  |  : ${Cyan}:  |  ${LgBlink}.${Normal} ${Magenta}     , ' :| | :| ${LgBlink}.${Normal} : :         ${Normal}*${Cyan}   :${LgBlink}.${Normal}${Green}|| ${Normal}|
+|        *    *       ${Magenta},  |  : ${Cyan}:  |  ${Green}. ${Magenta}     , ' :| | :| . : :         ${Normal}*${Cyan}   :.${Green}|| ${Normal}|
 |             ${LgBlink}.${Normal},           ${Magenta} | |  ${Cyan}|  ${Green}: .:|  ${Magenta}     , | |${Cyan}| | : ${Green}|: |          | || ${Normal}|
-|      '          ${LgBlink}.${Normal}         ${Magenta}+ ,  |  ${Green}:  .: ${LgBlink}.${Normal} ${Magenta}        '| | ${Cyan}: :|${Green} :    ${Normal}${LgBlink}.${Normal}   ${Magenta}|:${Cyan}| |${Green}| ${Normal}|
-|         ${LgBlink}.${Normal}                 ${LgBlink}.${Normal}    ${Magenta}, ${Cyan}*|  |${Green}| : ${Normal}      ,${Magenta}    | | :| | :      |:| |  ${Normal}|
-| ${LgBlink}.${Normal}                ${LgBlink}.${Normal}          ${LgBlink}.${Normal}        ${Magenta}|| ${Green}|${LgBlink}.${Normal}: ${Normal}*${Magenta}          | || : :     :||||   ${Normal}|
-|        ${LgBlink}.${Normal}            ${LgBlink}.${Normal}   . *    ${LgBlink}.${Normal}   ${LgBlink}.${Normal}  ${Magenta}, |||${LgBlink}.${Normal}  ${Normal}+        +${Magenta} '| |||  ${LgBlink}.${Normal}  ||,|    ${Normal}|
-|     ${LgBlink}.${Normal}             *              ${LgBlink}.${Normal}     ${Magenta}+:,|${Green}| ${Magenta}            ${LgBlink}.${Normal} ||||  :${LgBlink}.${Normal}||,|     ${Normal}|
-| +                      ${LgBlink}.${Normal}                ${Magenta}${LgBlink}.${Normal}${LgBlink}.${Normal}||${Normal}*${Magenta}          ${LgBlink}.${Normal} | :,||+ |||,|      ${Normal}|
-|     ${LgBlink}.${Normal}                         +      ${Magenta}: ${Cyan}||${Green}|,${Magenta}        ${LgBlink}.${Normal}| :| | | |${LgBlink}.${Normal}| ||,     ${Normal}${LgBlink}.${Normal}  ${Normal}|
-|       *     +   '               +  ${Magenta}:|${Green}| |, ${Magenta}    :${LgBlink}.${Normal}+${LgBlink}.${Normal} || || | |:,|| ,          ${Normal}|
-|                            ${LgBlink}.${Normal}      ${Magenta}${LgBlink}.${Normal}|${Green}|, ${LgBlink}.${Normal}${Magenta}    ${LgBlink}.${Normal}${LgBlink}.${Normal}|| | |: ', ,| | |,  ${Normal}+         ${Normal}|
-|  ${LgBlink}.${Normal}       + +                      ${Magenta}||  ${Magenta}      |||: ,       :| |               ${Normal}|
-|              +         ${LgBlink}.${Normal}      ${LgBlink}.${Normal}    ${Magenta}| ${Green}${LgBlink}.${Normal}  ${Magenta}    ,|||${LgBlink}.${Normal}:      ${LgBlink}.${Normal}||    ${Normal}${LgBlink}.${Normal}      ${LgBlink}.${Normal}    ,${Normal}|
-|          '                           ${Magenta},|${Green}${LgBlink}.${Normal} ${Magenta}  ${LgBlink}.${Normal}  ,:|||   + ||'     ${Normal},           ${Normal}|
-|  ${DGray}__    ${Nornal}+      *                         ${Green},'${Magenta}       ,'|${LgBlink}.${Normal}    ,:                 ${Normal}|
-|${DGray}^,  ,---^^^----....____,..^---,^,,----.,.___          ${Cyan},${LgBlink}.${Normal}    ,${LgBlink}.${Normal}${Normal}  ${LgBlink}.${Normal}    ____,${LgBlink}.${Normal},-${Normal}|
+|      '          ${LgBlink}.${Normal}         ${Magenta}+ ,  |  ${Green}:  .: . ${Magenta}        '| | ${Cyan}: :|${Green} :    ${Normal}${LgBlink}.${Normal}   ${Magenta}|:${Cyan}| |${Green}| ${Normal}|
+|         ${LgBlink}.${Normal}                 ${LgBlink}.${Normal}    ${Magenta}, ${Cyan}*|  |${Green}| : ${Normal}      ,${Magenta}    | | ${Cyan}:| ${Green}| :      ${Magenta}|:${Cyan}| ${Green}|  ${Normal}|
+| ${LgBlink}.${Normal}                ${LgBlink}.${Normal}          ${LgBlink}.${Normal}        ${Magenta}|| ${Green}|.: ${Normal}*${Magenta}          | |${Cyan}| : ${Green}:     ${Magenta}:|${Cyan}||${Green}|   ${Normal}|
+|        ${LgBlink}.${Normal}            ${LgBlink}.${Normal}   . *    ${LgBlink}.${Normal}   ${LgBlink}.${Normal}  ${Magenta}, |||.  ${Normal}+        +${Magenta} '| ${Cyan}||${Green}|  ${LgBlink}.${Normal}  ${Magenta}||${Cyan},${Green}|    ${Normal}|
+|     ${LgBlink}.${Normal}             *              ${LgBlink}.${Normal}     ${Magenta}+:,|${Green}| ${Magenta}            ${LgBlink}.${Normal} ${Magenta}||${Cyan}||  ${Magenta}:${LgBlink}.${Normal}${Magenta}|${Cyan}|,${Green}|     ${Normal}|
+| +                      ${LgBlink}.${Normal}                ${Magenta}..${Magenta}|${Cyan}|${Normal}*${Magenta}          . | :${Cyan},|${Green}|+ ${Magenta}|||${Cyan},${Green}|      ${Normal}|
+|     ${LgBlink}.${Normal}                         +      ${Magenta}: ${Cyan}||${Green}|,${Magenta}        .| :| ${Cyan}| | ${Green}|.| ${Magenta}|${Cyan}|${Green},     ${Normal}${LgBlink}.${Normal}  ${Normal}|
+|       *     +   '               +  ${Magenta}:|${Green}| |, ${Magenta}    :.${Cyan}+. ||${Green} || | ${Magenta}|:,${Cyan}|| ${Green},          ${Normal}|
+|                            ${LgBlink}.${Normal}      ${Magenta}.|${Green}|, .${Magenta}    ..|${Cyan}| | ${Green}|: ${Cyan}', ,| ${Magenta}| ${Cyan}|${Green},  ${Normal}+         ${Normal}|
+|  ${LgBlink}.${Normal}       + +                      ${Magenta}||  ${Magenta}      ||${Cyan}|: ${Green},       ${Magenta}:${Cyan}| ${Green}|               ${Normal}|
+|              +         ${LgBlink}.${Normal}      ${LgBlink}.${Normal}    ${Magenta}| ${Green}.  ${Magenta}    ,|${Cyan}||${Green}.:      ${Magenta}.${Cyan}|${Green}|    ${Normal}${LgBlink}.${Normal}      ${LgBlink}.${Normal}    ,${Normal}|
+|          '                           ${Magenta},|${Green}. ${Magenta}  ${LgBlink}.${Normal}${Magenta}  ,:${Cyan}|${Green}||   ${Normal}+ ${Magenta}|${Cyan}|${Green}'     ${Normal},           ${Normal}|
+|  ${DGray}__    ${Normal}+      *                         ${Green},'${Magenta}       ,${Cyan}'${Green}|.    ${Magenta},${Green}:                 ${Normal}|
+|${DGray}^,  ,---^^^----....____,..^---,^,,----.,.___          ${Cyan},${Green}.    ${Cyan},${Green}.  .    ${DGray}____,.,-${Normal}|
 |${DGray}::::___,---^^,---^   ^  ^ ^::::::::^   ::::^^^---,..___ __,..---^^::::::::   ${Normal}|
 |${DGray}--^ ::::::  :^::::::::::::::::::::: ^  ::::::::::::::::: :::  ,,--..,__ :::  ${Normal}|
 \-----------------------------------------------------------------------------/
 """
+}
+#display all processes
+wmic process get procesid,commandline
+
+#display logical drives
+wmic logicaldisk get description,name
+
+#shows the amount of times the current user has logged on
+#can be subsitituted with other user names
+user=$(echo %USERNAME%)
+wmic netlogin where (name like "%$user%") ge
+
+#search password registry for password
+reg query HKLM if /fd password /t REG_SZ /s
